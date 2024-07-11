2b6b6560f62e441434806c3e2bf23569c2c8baae
bst82 <bt7355711982@gmail.com>
2024-07-11 19:53:09 +0530

    updated baner section

--- src/app/components/home-section/home-section.component.ts
+++ src/app/components/home-section/home-section.component.ts
@@ -1,11 +1,12 @@
 import { Component, Inject, PLATFORM_ID, OnInit } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
+import { NgOptimizedImage } from '@angular/common';
 import AOS from "aos";
 
 @Component({
   selector: 'app-home-section',
   standalone: true,
-  imports: [],
+  imports: [ NgOptimizedImage],
   templateUrl: './home-section.component.html',
   styleUrls: ['./home-section.component.scss'] // corrected 'styleUrl' to 'styleUrls'
 })
@@ -13,9 +14,15 @@ export class HomeSectionComponent implements OnInit {
 
   constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
 
+  
+
   ngOnInit(): void {
     if (isPlatformBrowser(this.platformId)) {
-      AOS.init();
+      AOS.init(
+        {
+          duration: 2000, 
+        }
+      );
     }
   }
 }
