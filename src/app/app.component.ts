0226542b9a75d3c4f94ab26cf23c456fa93eb0a9
bst82 <bt7355711982@gmail.com>
2024-07-11 14:51:47 +0530

    update import for AOS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,14 @@
-import { Component, Inject, PLATFORM_ID } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { isPlatformBrowser } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { HomeSectionComponent } from "./components/home-section/home-section.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeSectionComponent,RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
-
-  ngOnInit() {
-    if (isPlatformBrowser(this.platformId)) {
-      import('aos').then(AOS => {
-        AOS.init({
-          duration: 1200,
-          once: true,
-          easing: 'ease-in-out',
-          offset: 100
-        });
-      });
-    }
-  }
-  
+  title = 'meAndMyPandit';
 }
---
+++ src/app/components/home-section/home-section.component.ts
@@ -0,0 +1,21 @@
+import { Component, Inject, PLATFORM_ID, OnInit } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import AOS from "aos";
+
+@Component({
+  selector: 'app-home-section',
+  standalone: true,
+  imports: [],
+  templateUrl: './home-section.component.html',
+  styleUrls: ['./home-section.component.scss'] // corrected 'styleUrl' to 'styleUrls'
+})
+export class HomeSectionComponent implements OnInit {
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      AOS.init();
+    }
+  }
+}
