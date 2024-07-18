d195f7b4235bb67cd47f9f15faa4449eb6a4abd5
bst82 <bt7355711982@gmail.com>
2024-07-19 00:02:31 +0530

    Contact section update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,12 @@ import { SanskarComponent } from "./components/sanskar/sanskar.component";
 import { BenefitsComponent } from "./components/benefits/benefits.component";
 import { SubscriptionComponent } from "./components/subscription/subscription.component";
 import { OurWorkComponent } from "./components/our-work/our-work.component";
+import { ContactFormComponent } from "./components/contact-form/contact-form.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeSectionComponent, RouterLink, TempleComponent, SanskarComponent, BenefitsComponent, SubscriptionComponent, OurWorkComponent],
+  imports: [RouterOutlet, HomeSectionComponent, RouterLink, TempleComponent, SanskarComponent, BenefitsComponent, SubscriptionComponent, OurWorkComponent, ContactFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
