42cbd977eb42aea3f6149cb9b96b75cabb9f461c
Bhagyashri Tamrakar <86471670+BST82@users.noreply.github.com>
2024-12-07 17:55:48 +0530

    update new changes

--- src/app/components/components-modules/components-modules.module.ts
+++ src/app/components/components-modules/components-modules.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeSectionComponent } from '../home-section/home-section.component';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactFormComponent } from '../contact-form/contact-form.component';
+
 
 const routes: Routes = [
   { path: '', component: HomeSectionComponent  }, 
+  { path: 'contact', component: ContactFormComponent }, 
 ];
 
 @NgModule({
